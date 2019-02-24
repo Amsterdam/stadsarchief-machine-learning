f5dfcb2d498cee3c87d1f0c14312d5675fa52845
RM <RRMoelker@users.noreply.github.com>
2019-02-24 21:33:30 +0100

    minor changes

--- stats.py
+++ stats.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def convert_to_index(Y, types):
@@ -26,3 +27,25 @@ def list_stats(Y):
 
     print(f"score to beat: {largest_class[1] / total_count}")
 
+
+def show_train_curves(history):
+    """
+    based on: https://www.learnopencv.com/image-classification-using-convolutional-neural-networks-in-keras/
+    """
+    # Loss Curves
+    plt.figure(figsize=[8,6])
+    plt.plot(history.history['loss'],'r',linewidth=3.0)
+    plt.plot(history.history['val_loss'],'b',linewidth=3.0)
+    plt.legend(['Training loss', 'Validation Loss'],fontsize=18)
+    plt.xlabel('Epochs ',fontsize=16)
+    plt.ylabel('Loss',fontsize=16)
+    plt.title('Loss Curves',fontsize=16)
+
+    # Accuracy Curves
+    plt.figure(figsize=[8,6])
+    plt.plot(history.history['acc'],'r',linewidth=3.0)
+    plt.plot(history.history['val_acc'],'b',linewidth=3.0)
+    plt.legend(['Training Accuracy', 'Validation Accuracy'],fontsize=18)
+    plt.xlabel('Epochs ',fontsize=16)
+    plt.ylabel('Accuracy',fontsize=16)
+    plt.title('Accuracy Curves',fontsize=16)
