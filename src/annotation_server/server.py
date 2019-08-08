ca0229ec2810022d2be59868ec59c0c8366f36e1
RM <RRMoelker@users.noreply.github.com>
2019-08-08 10:53:09 +0200

    Images served from annotation server

--- src/annotation_server/server.py
+++ src/annotation_server/server.py
@@ -1,47 +1,51 @@
 """
 Flask app allowing annotations of yaml label files
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from flask_cors import CORS
-import os
+import logging
 from .LabelData import LabelData
 from predict.iiif_url import get_image_url
 
-label_dir = os.environ.get('LABEL_DIR')
-assert label_dir, 'path to label dir required (directory with <id>.yaml files)'
-
-iiif_api_root = os.environ.get('IIIF_API_ROOT')
-assert iiif_api_root
+log = logging.getLogger(__name__)
 
 
-data = LabelData(label_dir)
+def create_app(iiif_api_root, label_dir, image_dir):
+    data = LabelData(label_dir)
 
-app = Flask(__name__)
-CORS(app)
+    app = Flask(__name__)
+    CORS(app)
 
+    @app.route('/')
+    def get_ids():
+        ids = data.list_ids()
+        return jsonify({'ids': ids})
 
-@app.route('/')
-def get_ids():
-    ids = data.list_ids()
-    return jsonify({'ids': ids})
+    @app.route('/<id>')
+    def get_single(id):
+        item = data.get_example(id)
+        dim = [1200, 1200]
+        stadsdeel_code = item.get('stadsdeel_code')
+        dossier_nummer = item.get('dossier_nummer')
+        filename = f'{id}.jpg'
+        url = get_image_url(iiif_api_root, stadsdeel_code, dossier_nummer, filename, dim)
+        return jsonify({
+            'url': url,
+            'meta': item
+        })
 
+    @app.route('/<id>', methods=['PUT'])
+    def change_annotation(id):
+        content = request.json
+        result = data.update_example(id, content)
+        return jsonify(result)
 
-@app.route('/<id>')
-def get_single(id):
-    item = data.get_example(id)
-    dim = [1200, 1200]
-    stadsdeel_code = item.get('stadsdeel_code')
-    dossier_nummer = item.get('dossier_nummer')
-    filename = f'{id}.jpg'
-    url = get_image_url(iiif_api_root, stadsdeel_code, dossier_nummer, filename, dim)
-    return jsonify({
-        'url': url,
-        'meta': item
-    })
+    if image_dir:
+        log.info(f'Serving images from {app.root_path}/{image_dir}')
+        
+        @app.route('/<basename>.jpg')
+        def get_image(basename):
+            filename = f'{basename}.jpg'
+            return send_from_directory(image_dir, filename)
 
-
-@app.route('/<id>', methods=['PUT'])
-def change_annotation(id):
-    content = request.json
-    result = data.update_example(id, content)
-    return jsonify(result)
+    return app
--- src/run_annotation_server.py
+++ src/run_annotation_server.py
@@ -2,7 +2,10 @@
 Flask app allowing annotations of yaml label files
 """
 import logging
+import os
 import sys
+
+from annotation_server.server import create_app
 
 log_level = logging.DEBUG
 root = logging.getLogger()
@@ -11,4 +14,12 @@ handler = logging.StreamHandler(sys.stdout)
 handler.setLevel(log_level)
 root.addHandler(handler)
 
-from annotation_server.server import app
+iiif_api_root = os.environ.get('IIIF_API_ROOT')
+assert iiif_api_root
+
+label_dir = os.environ.get('LABEL_DIR')
+assert label_dir, 'path to label dir required (directory with <id>.yaml files)'
+
+image_dir = os.environ.get('IMAGE_DIR')  # path from Flask root_path (which is from this python file directory), not working directory!
+
+app = create_app(iiif_api_root, label_dir, image_dir)
